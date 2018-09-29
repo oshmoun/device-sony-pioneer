545dd79a6a0d566dd8ff04bec9c6afee5e5e77d2
oshmoun <oshmoun100@googlemail.com>
2018-09-29 02:34:07 +0200

    omni: move gsm.mk include to the top
    Change-Id: Iafb669aa2954eb6c2c8b7eca34a11f7b9a893e05
    Signed-off-by: MarijnS95 <marijns95@gmail.com>

--- omni_pioneer.mk
+++ omni_pioneer.mk
@@ -15,6 +15,9 @@
 # Bootanimation
 TARGET_BOOTANIMATION_SIZE := 1080p
 
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit device parts
 $(call inherit-product, device/sony/pioneer/aosp_h3113.mk)
 
@@ -27,4 +30,3 @@ TARGET_OTA_ASSERT_DEVICE := pioneer
 
 # Inherit rom parts
 $(call inherit-product, device/sony/common/omni.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
